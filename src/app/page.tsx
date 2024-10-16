052446754a392b45e208e80aa1c650f5473fd4a8
raffay <you@example.com>
2024-10-16 05:23:26 -0700

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-
 import React from 'react'
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Features from "./Components/Features";
 import Company from "./Components/Company";
 import Cards from "./Components/Cards";
-import Cards2 from "./Components/TwoCards";
 import TwoCards from "./Components/TwoCards";
 import HR from "./Components/HR";
 import Exp from "./Components/Exp";
