7ef12ab6788c61a314a02c3ed0e83a208145c6c0
raffay <you@example.com>
2024-10-17 23:54:14 -0700

    chnaged

--- src/app/Components/FAQ.tsx
+++ src/app/Components/FAQ.tsx
@@ -1,15 +1,14 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import "aos/dist/aos.css";
-import Aos from "aos";
+'use client';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import 'aos/dist/aos.css';
+import Aos from 'aos';
 
 const FAQSection = () => {
   useEffect(() => {
     Aos.init({
       duration: 300, // Minimized animation duration
-      easing: "ease-in-out",
+      easing: 'ease-in-out',
       once: false,
     });
   }, []);
@@ -19,34 +18,34 @@ const FAQSection = () => {
 
   const faqItems = [
     {
-      id: "answer-1",
-      question: "How easy is it to set up the software?",
+      id: 'answer-1',
+      question: 'How easy is it to set up the software?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.',
     },
     {
-      id: "answer-2",
-      question: "What kind of support is available?",
+      id: 'answer-2',
+      question: 'What kind of support is available?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.',
     },
     {
-      id: "answer-3",
-      question: "Is the software compatible with other tools I use?",
+      id: 'answer-3',
+      question: 'Is the software compatible with other tools I use?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.',
     },
     {
-      id: "answer-4",
-      question: "How does the pricing work?",
+      id: 'answer-4',
+      question: 'How does the pricing work?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.',
     },
     {
-      id: "answer-5",
-      question: "Can I customize the features to fit my needs?",
+      id: 'answer-5',
+      question: 'Can I customize the features to fit my needs?',
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris fusce nec tellus sed augue semper porta.',
     },
   ];
 
@@ -56,7 +55,7 @@ const FAQSection = () => {
   };
 
   return (
-    <div  className="bg-[#E6F3FF] dark:bg-[#1a1a1a] py-6 ">
+    <div className="bg-[#E6F3FF] dark:bg-[#1a1a1a] py-6 ">
       <div className="max-w-3xl mx-auto">
         {/* Added FAQs box */}
         <div className="w-[55px] h-[22px] mx-auto mb-8">
@@ -68,7 +67,7 @@ const FAQSection = () => {
           Frequently Asked Questions
         </h1>
         <div className="space-y-4">
-          {faqItems.map((item, index) => (
+          {faqItems.map((item) => (
             <div
               key={item.id}
               className="bg-white dark:bg-[#333333] rounded-lg shadow-md overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" // Added hover effects
@@ -81,7 +80,9 @@ const FAQSection = () => {
                   {item.question}
                 </span>
                 <svg
-                  className={`w-5 h-5 text-gray-500 dark:text-gray-300 ${openedAnswer === item.id ? "rotate-180" : ""}`} // Rotate icon based on opened state
+                  className={`w-5 h-5 text-gray-500 dark:text-gray-300 ${
+                    openedAnswer === item.id ? 'rotate-180' : ''
+                  }`} // Rotate icon based on opened state
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -95,9 +96,11 @@ const FAQSection = () => {
                 </svg>
               </button>
               <div
-                className={`px-4 pb-4 text-gray-600 dark:text-gray-300 transition-all duration-300 ease-in-out ${openedAnswer === item.id ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}`}
+                className={`px-4 pb-4 text-gray-600 dark:text-gray-300 transition-all duration-300 ease-in-out ${
+                  openedAnswer === item.id ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'
+                }`}
                 style={{
-                  overflow: "hidden", // Ensure overflow is hidden for smooth transition
+                  overflow: 'hidden', // Ensure overflow is hidden for smooth transition
                 }}
               >
                 {item.answer}
@@ -111,5 +114,6 @@ const FAQSection = () => {
 };
 
 export default FAQSection;
+
 
 
--- src/app/Components/Pricing.tsx
+++ src/app/Components/Pricing.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Check } from 'lucide-react'
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 export default function PricingPlans() {
